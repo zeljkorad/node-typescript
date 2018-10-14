a257bb8787fdb367f362510ed79fc035a0ed2282
Zeljko Radic <zeljko@amerbank.com>
2018-10-14 17:36:53 +0200

    code refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,24 @@
-import * as express from 'express'
+import * as express from 'express';
 
 class App {
   public express
 
   constructor () {
-    this.express = express()
-    this.mountRoutes()
+    this.express = express();
+    this.mountRoutes();
   }
 
   private mountRoutes (): void {
-    const router = express.Router()
+    const router = express.Router();
+
     router.get('/', (req, res) => {
       res.json({
-        message: 'Hello World!'
+        message: 'Node.js starter!'
       })
-    })
-    this.express.use('/', router)
+    });
+
+    this.express.use('/', router);
   }
 }
 
-export default new App().express
+export default new App().express;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import app from './app'
+import app from './app';
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, (err) => {
   if (err) {
     return console.log(err)
   }
 
-  return console.log(`server is listening on ${port}`)
-})
+  return console.log(`server is listening on ${port}`);
+});
